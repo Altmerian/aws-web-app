056ed5e320d070c73ce6c8513d79542b1e37360c
Pavel Shakhlovich <pavel_shakhlovich@epam.com>
2021-12-18 17:30:15 +0300

    initial

---
+++ src/main/java/com/epam/cloudx/pavelsh/awsapp/AwsAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.cloudx.pavelsh.awsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/cloudx/pavelsh/awsapp/AwsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.cloudx.pavelsh.awsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
