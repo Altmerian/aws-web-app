88c2d282c9ff2ac937648be86a0065b0589fa2b8
Pavel Shakhlovich <pavel_shakhlovich@epam.com>
2022-01-19 00:27:40 +0300

    db url

--- task-7-rds/src/main/java/com/epam/cloudx/pavelsh/awsapp/rds/constant/Constants.java
+++ task-7-rds/src/main/java/com/epam/cloudx/pavelsh/awsapp/rds/constant/Constants.java
@@ -1,7 +1,7 @@
 package com.epam.cloudx.pavelsh.awsapp.rds.constant;
 
 public class Constants {
-  public static final String S3_BUCKET_NAME = "bucket name";
+  public static final String S3_BUCKET_NAME = "images";
   public static final String SNS_TOPIC_ARN = "arn topic";
   public static final String SQS_TOPIC_URL = "sqs topic";
 }
